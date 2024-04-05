7ffe89e9860373a2c14f96d4264db77a9aac86e5
nnthanh01061999 <82963086+nnthanh01061999@users.noreply.github.com>
2024-04-05 18:01:01 +0700

    feat: add PWA config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const withPWA = require("@ducanh2912/next-pwa").default({
+  dest: "public",
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  workboxOptions: {
+    disableDevLogs: true,
+  },
+});
 
-module.exports = nextConfig
+module.exports = withPWA({
+  // Your Next.js config
+});
